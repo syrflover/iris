ecebd89ecec9a2a1a3668a83b283567d2765edf9
Lee Tae Woo <syr@meu.works>
2019-06-29 02:12:12 +0900

    add commandParser

---
+++ src/lib/commandParser.ts
@@ -0,0 +1,70 @@
+export const enum Flag {
+    float,
+    int,
+    string,
+    // void,
+}
+
+export interface IFlags {
+    [flag: string]: {
+        type: Flag;
+        default: any;
+    };
+}
+
+export interface IBaseCommandParseResult {
+    content: string;
+}
+
+// export type Flags = [string, Flag][];
+
+const getParameter = (param: string, flag: { name: string; type: Flag }) => {
+    const trimp = param.trim();
+
+    switch (flag.type) {
+        case Flag.float:
+            const float = parseFloat(trimp);
+
+            if (isNaN(float)) {
+                throw new Error(`${flag.name} must be float`);
+            }
+
+            return float;
+        case Flag.int:
+            const int = parseInt(trimp, 10);
+
+            if (isNaN(int)) {
+                throw new Error(`${flag.name} must be int`);
+            }
+
+            return int;
+        case Flag.string:
+            return trimp;
+        /*  case Flag.void:
+              return ''; */
+    }
+};
+
+export const commandParser = (content: string, flags: IFlags) => {
+    const st = content.split(' ').filter((e) => e.replace(/ /g, ''));
+
+    const r: { [flag: string]: any; content: string } = { content };
+
+    for (const flag in flags) {
+        const flagIndex = st.findIndex((s) => s === `--${flag}`);
+
+        const hasNotFlag = flagIndex === -1;
+
+        const parameter = hasNotFlag
+            ? flags[flag].default
+            : getParameter(st[flagIndex + 1], {
+                  name: flag,
+                  type: flags[flag].type,
+              });
+
+        r[flag] = parameter;
+        r.content = r.content.replace(`--${flag} ${parameter}`, '').trim();
+    }
+
+    return r;
+};
