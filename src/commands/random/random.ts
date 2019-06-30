202f32aac453996bb454138bdab6359960edd0ce
Lee Tae Woo <syr@meu.works>
2019-06-30 09:16:02 +0900

    random bug fix

--- src/commands/random/random.ts
+++ src/commands/random/random.ts
@@ -4,6 +4,7 @@ import { Message } from 'discord.js';
 
 import { CommandFunc } from '../index';
 import { IBaseCommandParseResult } from '../../lib/commandParser';
+import { StateError } from '../../state';
 
 export const random: CommandFunc = (
     { content }: IBaseCommandParseResult,
@@ -13,6 +14,14 @@ export const random: CommandFunc = (
         const t = content.split(' ');
 
         const [a, ...b] = t.map((e) => parseInt(e, 10));
+
+        // end - begin < 1073741824 occurs error
+        const er = isNaN(b[0]) ? a > 1073741824 : b[0] - a > 1073741824;
+
+        if (er) {
+            reject(new StateError('end - begin < 1073741824 occurs error', message));
+            return;
+        }
 
         try {
             const r = F.isNil(a)
