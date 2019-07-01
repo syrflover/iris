4a97af9cc5a12a36d35b9c9f0ab9bf2cd6ee77a3
Lee Tae Woo <syr@meu.works>
2019-07-01 16:59:23 +0900

    Update spawnp.ts

--- src/lib/spawnp.ts
+++ src/lib/spawnp.ts
@@ -1,14 +1,16 @@
 import { spawn } from 'child_process';
 
-export const spawnp = (c: string, a: string[]): Promise<string> =>
+export const spawnp = (c: string, a: string[]): Promise<{ result: string; time: number }> =>
     new Promise((resolve, reject) => {
+        const begin = Date.now();
+
         const cmd = spawn(c, a);
 
         let stdout = '';
         let stderr = '';
 
         cmd.stdout.on('data', (data: Buffer) => {
-            stdout += data.toString();
+            stdout += `${data.toString()}\n`;
         });
 
         cmd.stdout.on('error', (e) => {
@@ -17,11 +19,18 @@ export const spawnp = (c: string, a: string[]): Promise<string> =>
         });
 
         cmd.stderr.on('data', (d) => {
-            stderr += d.toString();
+            stderr += `${d.toString()}\n`;
         });
 
         cmd.on('exit', () => {
-            resolve(stderr.trim().length > 0 ? stderr : stdout);
+            const end = Date.now();
+
+            const r = {
+                result: stderr.trim().length > 0 ? stderr : stdout,
+                time: end - begin,
+            };
+
+            resolve(r);
         });
 
         cmd.stderr.on('error', (e) => {
