45aecde4787d8a77e631909e9c6025c881a6a93c
Lee Tae Woo <syr@meu.works>
2019-07-01 17:00:58 +0900

    change updat command

--- src/commands/update/update.ts
+++ src/commands/update/update.ts
@@ -14,18 +14,22 @@ export const update: CommandFunc<IBaseCommandParseResult> = (
 ): Promise<void> =>
     new Promise(async (resolve, reject) => {
         try {
-            const updateLog = await spawnp('git', ['pull']);
+            const { result: updateLog, time: gitPullTime } = await spawnp('git', ['pull']);
 
             const alreadyUpToDate = 'Already up to date.' === updateLog.trim();
 
-            await message.channel.send(alreadyUpToDate ? '이미 최신 버전이에요' : updateLog);
+            await message.channel.send(`${alreadyUpToDate ? '이미 최신 버전이에요' : updateLog}`);
 
             resolve();
 
             if (!alreadyUpToDate && env.NODE_ENV !== 'development') {
                 const pm = (await shouldUseYarn()) ? 'yarn' : 'npm';
 
-                await spawnp(pm, ['run', 'build']);
+                await message.channel.send(`업데이트 받는데 ${gitPullTime}ms 만큼 걸렸어요`);
+
+                const { time: buildTime } = await spawnp(pm, ['run', 'build']);
+
+                await message.channel.send(`빌드 하는데 ${buildTime}ms 만큼 걸렸어요`);
 
                 await message.channel.send('재시작 할 거예요');
 
