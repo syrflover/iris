983ae7144c207a47e85a917b4f5deaea997b68b0
Lee Tae Woo <syr@meu.works>
2019-07-11 23:46:50 +0900

    dotenv to json

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,17 @@
-import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
-export interface IEnv extends NodeJS.ProcessEnv {
+export interface IEnv {
     DISCORD_TOKEN: string;
+    OWNER_ID: string;
+    USE_SSH_IN_SH: boolean;
+    SSH_KEY_PATH: string;
 }
 
 const getEnv = () => {
-    dotenv.config();
+    const e = fs.readFileSync('./.env.json', { encoding: 'utf8' });
+    const parsed = JSON.parse(e);
 
-    return process.env as IEnv;
+    return parsed as IEnv;
 };
 
 export const env = getEnv();
