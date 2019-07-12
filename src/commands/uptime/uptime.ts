19bbe3096010f4c503e2859c7c03840c4271d76c
Lee Tae Woo <syr@meu.works>
2019-07-12 23:35:28 +0900

    remove todo comment

--- src/commands/uptime/uptime.ts
+++ src/commands/uptime/uptime.ts
@@ -11,7 +11,6 @@ export const uptime: CommandFunc<IBaseCommandParseResult> = (
 ): Promise<void> =>
     new Promise(async (resolve, reject) => {
         try {
-            // TODO: Change Format to 1 day 1 hour 12 minutes 32 seconds
             await message.channel.send(ms(process.uptime() * 1000));
             resolve();
         } catch (error) {
