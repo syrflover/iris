b557c3c3f0e50f55609561cc7f936e77eeab55ca
Lee Tae Woo <syr@meu.works>
2019-07-13 00:22:16 +0900

    use mkdirp

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,14 +1,14 @@
 import { client } from './client';
 import { env } from './env';
 import { paths } from './lib/resolvePath';
-import { mkdir } from './lib/mkdir';
 import { sayStore } from './store/sayStore';
 import { stateStore } from './store/stateStore';
+import { mkdirp } from 'simply-store/build/lib/fs';
 
 const bootstrap = async () => {
     try {
         // say cache directory
-        await mkdir(paths.cache);
+        await mkdirp(paths.cache);
 
         // say session file
         await sayStore.initialize({});
--- src/lib/mkdir.ts
+++
@@ -1,4 +0,0 @@
-import * as fs from 'fs';
-
-export const mkdir = async (p: string) =>
-    !fs.existsSync(p) ? await fs.promises.mkdir(p) : undefined;
