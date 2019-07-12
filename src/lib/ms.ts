2d7fd59b1cc5a9b0242f9f99b72fd226a6095b5e
Lee Tae Woo <syr@meu.works>
2019-07-12 21:41:31 +0900

    add src/lib/ms.ts and test

---
+++ src/lib/ms.ts
@@ -0,0 +1,41 @@
+import mss = require('ms');
+
+export const SECOND = 1000;
+export const MINUTE = 60000;
+export const HOUR = 3600000;
+export const DAY = 86400000;
+
+export const ms = (milliseconds: number) => {
+    let mt = milliseconds;
+    let result = '';
+
+    if (mt >= DAY) {
+        const t = Math.floor(mt / DAY) * DAY;
+
+        result += `${mss(t, { long: true })} `;
+        mt -= t;
+    }
+
+    if (mt >= HOUR) {
+        const t = Math.floor(mt / HOUR) * HOUR;
+
+        result += `${mss(t, { long: true })} `;
+        mt -= t;
+    }
+
+    if (mt >= MINUTE) {
+        const t = Math.floor(mt / MINUTE) * MINUTE;
+
+        result += `${mss(t, { long: true })} `;
+        mt -= t;
+    }
+
+    if (mt >= SECOND) {
+        const t = Math.floor(mt / SECOND) * SECOND;
+
+        result += `${mss(t, { long: true })} `;
+        mt -= t;
+    }
+
+    return `${result.trim()} ${mt > 0 ? `${mt} milliseconds` : ''}`.trim();
+};
---
+++ test/lib/ms.test.ts
@@ -0,0 +1,69 @@
+import { assert } from 'chai';
+
+import { ms, DAY, HOUR, MINUTE, SECOND } from '../../src/lib/ms';
+
+describe('test src/lib/ms.ts', () => {
+    it('day / days, no milliseconds', () => {
+        const r0 = ms(DAY);
+        const r1 = ms(DAY * 2);
+
+        assert.strictEqual(r0, '1 day');
+        assert.strictEqual(r1, '2 days');
+    });
+
+    it('day / days, milliseconds', () => {
+        const r0 = ms(DAY + 720);
+        const r1 = ms(DAY * 2 + 720);
+
+        assert.strictEqual(r0, '1 day 720 milliseconds');
+        assert.strictEqual(r1, '2 days 720 milliseconds');
+    });
+
+    it('hour / hours, no milliseconds', () => {
+        const r0 = ms(HOUR);
+        const r1 = ms(HOUR * 2);
+
+        assert.strictEqual(r0, '1 hour');
+        assert.strictEqual(r1, '2 hours');
+    });
+
+    it('hour / hours, milliseconds', () => {
+        const r0 = ms(HOUR + 720);
+        const r1 = ms(HOUR * 2 + 720);
+
+        assert.strictEqual(r0, '1 hour 720 milliseconds');
+        assert.strictEqual(r1, '2 hours 720 milliseconds');
+    });
+
+    it('minute / minutes, no milliseconds', () => {
+        const r0 = ms(MINUTE);
+        const r1 = ms(MINUTE * 2);
+
+        assert.strictEqual(r0, '1 minute');
+        assert.strictEqual(r1, '2 minutes');
+    });
+
+    it('minute / minutes, milliseconds', () => {
+        const r0 = ms(MINUTE + 720);
+        const r1 = ms(MINUTE * 2 + 720);
+
+        assert.strictEqual(r0, '1 minute 720 milliseconds');
+        assert.strictEqual(r1, '2 minutes 720 milliseconds');
+    });
+
+    it('second / seconds, no milliseconds', () => {
+        const r0 = ms(SECOND);
+        const r1 = ms(SECOND * 2);
+
+        assert.strictEqual(r0, '1 second');
+        assert.strictEqual(r1, '2 seconds');
+    });
+
+    it('second / seconds, milliseconds', () => {
+        const r0 = ms(SECOND + 720);
+        const r1 = ms(SECOND * 2 + 720);
+
+        assert.strictEqual(r0, '1 second 720 milliseconds');
+        assert.strictEqual(r1, '2 seconds 720 milliseconds');
+    });
+});
