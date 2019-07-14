30e212cff23de7fcc8a564409be3cf4052dba793
Lee Tae Woo <syr@meu.works>
2019-07-15 03:33:02 +0900

    Update ms.test.ts

--- test/lib/ms.test.ts
+++ test/lib/ms.test.ts
@@ -4,12 +4,22 @@ import { ms, DAY, HOUR, MINUTE, SECOND } from '../../src/lib/ms';
 
 describe('test src/lib/ms.ts', () => {
     it('day hour minute second millisecond', () => {
-        const r = ms(DAY + HOUR + MINUTE + SECOND + 720);
+        const r0 = ms(DAY + HOUR + MINUTE + SECOND + 1);
+        const r1 = ms(DAY * 2 + HOUR * 2 + MINUTE * 2 + SECOND * 2 + 720);
 
-        assert.strictEqual(r, '1 day 1 hour 1 minute 1 second 720 milliseconds');
+        assert.strictEqual(r0, '1 day 1 hour 1 minute 1 second 1 millisecond');
+        assert.strictEqual(r1, '2 days 2 hours 2 minutes 2 seconds 720 milliseconds');
     });
 
-    it('day / days, no milliseconds', () => {
+    it('only millisecond / milliseconds', () => {
+        const r0 = ms(1);
+        const r1 = ms(720);
+
+        assert.strictEqual(r0, '1 millisecond');
+        assert.strictEqual(r1, '720 milliseconds');
+    });
+
+    it('day / days, without milliseconds', () => {
         const r0 = ms(DAY);
         const r1 = ms(DAY * 2);
 
@@ -17,7 +27,7 @@ describe('test src/lib/ms.ts', () => {
         assert.strictEqual(r1, '2 days');
     });
 
-    it('day / days, milliseconds', () => {
+    it('day / days, with milliseconds', () => {
         const r0 = ms(DAY + 720);
         const r1 = ms(DAY * 2 + 720);
 
@@ -25,7 +35,7 @@ describe('test src/lib/ms.ts', () => {
         assert.strictEqual(r1, '2 days 720 milliseconds');
     });
 
-    it('hour / hours, no milliseconds', () => {
+    it('hour / hours, without milliseconds', () => {
         const r0 = ms(HOUR);
         const r1 = ms(HOUR * 2);
 
@@ -33,7 +43,7 @@ describe('test src/lib/ms.ts', () => {
         assert.strictEqual(r1, '2 hours');
     });
 
-    it('hour / hours, milliseconds', () => {
+    it('hour / hours, with milliseconds', () => {
         const r0 = ms(HOUR + 720);
         const r1 = ms(HOUR * 2 + 720);
 
@@ -41,7 +51,7 @@ describe('test src/lib/ms.ts', () => {
         assert.strictEqual(r1, '2 hours 720 milliseconds');
     });
 
-    it('minute / minutes, no milliseconds', () => {
+    it('minute / minutes, without milliseconds', () => {
         const r0 = ms(MINUTE);
         const r1 = ms(MINUTE * 2);
 
@@ -49,7 +59,7 @@ describe('test src/lib/ms.ts', () => {
         assert.strictEqual(r1, '2 minutes');
     });
 
-    it('minute / minutes, milliseconds', () => {
+    it('minute / minutes, with milliseconds', () => {
         const r0 = ms(MINUTE + 720);
         const r1 = ms(MINUTE * 2 + 720);
 
@@ -57,7 +67,7 @@ describe('test src/lib/ms.ts', () => {
         assert.strictEqual(r1, '2 minutes 720 milliseconds');
     });
 
-    it('second / seconds, no milliseconds', () => {
+    it('second / seconds, without milliseconds', () => {
         const r0 = ms(SECOND);
         const r1 = ms(SECOND * 2);
 
@@ -65,7 +75,7 @@ describe('test src/lib/ms.ts', () => {
         assert.strictEqual(r1, '2 seconds');
     });
 
-    it('second / seconds, milliseconds', () => {
+    it('second / seconds, with milliseconds', () => {
         const r0 = ms(SECOND + 720);
         const r1 = ms(SECOND * 2 + 720);
 
