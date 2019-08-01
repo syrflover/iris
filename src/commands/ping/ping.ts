f220fc64ac81ce5bfbfd32cf75cd0ac2551fdba2
Lee Tae Woo <syr@meu.works>
2019-08-01 17:31:14 +0900

    seperated request func

--- src/commands/ping/ping.ts
+++ src/commands/ping/ping.ts
@@ -5,21 +5,7 @@ import { Message } from 'discord.js';
 
 import { CommandFunc } from '..';
 import { StateError } from '../../state';
-
-interface IRequestResult {
-    url: string;
-    latency: number;
-    begin: number;
-    response?: {
-        headers: { [index: string]: string };
-        status_code: number;
-        method: string;
-    };
-    error?: string;
-}
-
-const request = (url: string) =>
-    axios.post<IRequestResult[]>('https://request.syrflover.co', [url]).then((res) => res.data);
+import { request } from '../../lib/request';
 
 export const ping: CommandFunc<IBaseCommandParseResult> = (
     { content }: IBaseCommandParseResult,
@@ -27,7 +13,7 @@ export const ping: CommandFunc<IBaseCommandParseResult> = (
 ): Promise<void> =>
     new Promise(async (resolve, reject) => {
         try {
-            const { response, url, latency, error } = (await request(content))[0];
+            const { response, url, latency, error } = (await request([content]))[0];
 
             const result = error
                 ? `\`${url}\`  :arrow_right:  \`${error}\``
---
+++ src/lib/request.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+interface IRequestResult {
+    url: string;
+    latency: number;
+    begin: number;
+    response?: {
+        headers: { [index: string]: string };
+        status_code: number;
+        method: string;
+    };
+    error?: string;
+}
+
+export const request = (urls: string[]) =>
+    axios.post<IRequestResult[]>('https://request.syrflover.co', urls).then((res) => res.data);
