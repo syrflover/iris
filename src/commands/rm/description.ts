70d1dc5f69f5f1d8bc00dd7f5db0443b784d1449
Lee Tae Woo <syr@meu.works>
2019-10-04 00:24:51 +0900

    change rm impl

--- src/commands/rm/description.ts
+++ src/commands/rm/description.ts
@@ -1,2 +1,3 @@
-export const description = '메세지를 개수만큼 삭제하여요';
+export const description =
+    '메세지를 개수만큼 삭제하여요\n\n한 번에 삭제할 수 있는 메세지 갯수는 399개여요';
 // export const description = 'remove messages';
--- src/commands/rm/rm.ts
+++ src/commands/rm/rm.ts
@@ -1,14 +1,27 @@
+import * as F from 'nodekell';
 import { Message } from 'discord.js';
 
 import { CommandFunc } from '../index';
 import { StateError } from '../../state';
 import { IBaseCommandParseResult } from '@syrflover/command-parser';
+import { seperateN } from '../../lib/seperateN';
+
+const removeMessages = F.curry(async (message: Message, removeAmount: number) => {
+    const messages = await message.channel
+        .fetchMessages({
+            limit: removeAmount + 1,
+        })
+        .then((msgs) => msgs.filter((msg) => msg.id !== message.id));
+
+    await message.channel.bulkDelete(messages);
+});
 
 export const rm: CommandFunc<IBaseCommandParseResult> = (
     { content }: IBaseCommandParseResult,
     message: Message,
 ): Promise<void> =>
     new Promise(async (resolve, reject) => {
+        const limit = 400;
         const removeAmount = parseInt(content, 10);
 
         if (isNaN(removeAmount)) {
@@ -16,14 +29,16 @@ export const rm: CommandFunc<IBaseCommandParseResult> = (
             return;
         }
 
-        try {
-            const messages = await message.channel
-                .fetchMessages({
-                    limit: removeAmount + 1,
-                })
-                .then((msgs) => msgs.filter((msg) => msg.id !== message.id));
+        if (removeAmount >= limit) {
+            reject(new StateError(`삭제할 메세지 갯수는 ${limit}보다 작아야 하여요`, message));
+            return;
+        }
 
-            await message.channel.bulkDelete(messages);
+        try {
+            const seperatedBy99 = seperateN(removeAmount, 99);
+
+            await F.forEach(removeMessages(message), seperatedBy99);
+
             resolve();
         } catch (error) {
             reject(new StateError(error.message, message));
