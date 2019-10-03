7ac1ee7c8fdc7e3d621ebf6c524902fdd1c628ac
Lee Tae Woo <syr@meu.works>
2019-10-04 00:24:37 +0900

    add seperateN

---
+++ src/lib/seperateN.ts
@@ -0,0 +1,39 @@
+/**
+ * @param {Number} xs target
+ * @param {Number} n standard point
+ *
+ * @example
+ * const r = seperateN(230, 99);
+ * assert.deepStrictEqual([99, 99, 32], r);
+ */
+
+export const seperateN = async function* (
+    xs: number,
+    n: number,
+    r: number[] = [],
+): AsyncIterableIterator<number> {
+    const notOverN = xs <= n;
+
+    if (notOverN) {
+        yield* [...r, xs];
+    } else {
+        yield* seperateN(xs - n, n, [...r, 99]);
+    }
+};
+
+/* export const seperateN = (xs: number, n: number, r: number[] = []): Promise<number[]> =>
+    new Promise((resolve, reject) => {
+        const notOverN = xs <= n;
+
+        if (notOverN) {
+            resolve([...r, xs]);
+            return;
+        }
+        resolve(seperateN(xs - n, n, [...r, 99]));
+    }); */
+
+/* (async () => {
+    for await (const e of seperateN(230, 99)) {
+        console.log(e);
+    }
+})(); */
